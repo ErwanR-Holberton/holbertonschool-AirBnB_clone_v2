567869870b5a98597f7319ded25f1c3209a586d0
ErwanR-Holberton <6218@holbertonstudents.com>
2023-12-01 14:05:40 +0100

    Fixed bug in createtion of place

--- models/city.py
+++ models/city.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship as rel
 
@@ -10,4 +11,4 @@ class City(BaseModel, Base):
     __tablename__ = 'cities'
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    places = rel('Place', backref='user', cascade='all, delete-orphan')
+    places = rel('Place', backref='cities')
--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
+from models.place import Place
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship as rel
 
@@ -8,8 +9,9 @@ from sqlalchemy.orm import relationship as rel
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
     __tablename__ = 'users'
+    __table_args__ = {'extend_existing': True}
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
-    places = rel('Place', backref='user', cascade='all, delete-orphan')
+    places = rel('Place', backref='user')
